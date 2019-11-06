78f2973e2714cf57f1efa84c0a98745533530b66
Max Verchinskiy <disablaksteam@gmail.com>
2019-11-06 20:58:22 +0200

    update

---
+++ Assets/Scripts/SetActivePanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActivePanel : MonoBehaviour
+{
+  public void tumbler( GameObject panel )
+  {
+    panel.SetActive( !panel.activeSelf );
+  }
+}
