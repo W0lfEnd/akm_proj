1d228297de3ce582c9a68c901f687ac7f07a30d0
Max Verchinskiy <disablaksteam@gmail.com>
2019-11-14 19:58:25 +0200

    update

---
+++ Assets/Scripts/Game/ShipView/ShipViewManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipViewManager : MonoBehaviour
+{
+    [Header("Parallax")]
+    [SerializeField] private RectTransform[] backgrounds;
+    [SerializeField] private float speed_parallax = 1.0f;
+
+    private Vector2 size_background;
+    
+    void Start()
+    {
+        size_background = backgrounds[0].sizeDelta;
+    }
+
+    void Update()
+    {
+        parallaxEffext();
+    }
+
+    void parallaxEffext()
+    {
+        slowMoveBackground();
+    }
+
+    private void slowMoveBackground()
+    {
+        foreach ( RectTransform back in backgrounds )
+        {
+            back.Translate( speed_parallax * Time.deltaTime * Vector2.left );
+
+            if ( back.localPosition.x < -1600 )
+                back.localPosition = new Vector2(back.localPosition.x + (size_background.x * 2), back.localPosition.y );
+        }
+    }
+}
---
+++ Assets/Scripts/Game/SpaceShipPlaces/SpaceShipPlacesManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SpaceShipPlacesManager : MonoBehaviour
+{
+  [SerializeField] private Button[] btn_places;
+
+  private void Start()
+  {
+    setAllPlacesGreen();
+  }
+
+
+  public void changePlace( int idx_of_place )
+  {
+    changeColorPlace( idx_of_place );
+  }
+
+  private void changeColorPlace( int idx_of_place )
+  {
+    setAllPlacesGreen();
+    btn_places[idx_of_place].GetComponent<Image>().color = Color.black;
+  }
+
+  private void setAllPlacesGreen()
+  {
+    foreach ( Button btn in btn_places )
+      btn.GetComponent<Image>().color = Color.green;
+  }
+}
