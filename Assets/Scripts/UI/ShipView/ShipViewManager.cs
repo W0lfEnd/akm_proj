1f81b1b1bb861707385039cf131c1a069831954d
Andrii <oliinyk.dev@gmail.com>
2019-11-18 19:41:13 +0200

    added max branch

---
+++ Assets/Scripts/UI/ShipView/ShipViewManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipViewManager : MonoBehaviour
+{
+  [Header("Parallax")]
+  [SerializeField] private Transform controller = null;
+  [SerializeField] private Transform targe_test = null;
+  [SerializeField] private RectTransform[] backgrounds;
+  [SerializeField, Range(0.0f, 50.0f)] private float speed_parallax = 1.0f;
+
+  private Vector2 size_background;
+
+  void Start()
+  {
+    size_background = backgrounds[0].sizeDelta;
+    setRotation( targe_test );
+  }
+
+  void Update()
+  {
+    parallaxEffext();
+  }
+
+  void parallaxEffext()
+  {
+    slowMoveBackground();
+  }
+
+  private void slowMoveBackground()
+  {
+    foreach ( RectTransform back in backgrounds )
+    {
+      back.Translate( speed_parallax * Time.deltaTime * Vector2.left );
+
+      if ( back.localPosition.x < -1600 )
+        back.localPosition = new Vector2( back.localPosition.x + (size_background.x * 2), back.localPosition.y );
+    }
+  }
+
+  public void setRotation( Transform target )
+  {
+    Vector3 vectorToTarget = target.position - controller.position;
+    float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+    Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+    transform.rotation = q;
+  }
+}
---
+++ Assets/Scripts/UI/SpaceShipPlaces/SpaceShipPlacesManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SpaceShipPlacesManager : MonoBehaviour
+{
+  [SerializeField] private Button[] btn_places;
+
+  private void Start()
+  {
+    setAllPlacesGreen();
+  }
+
+
+  public void changePlace( int idx_of_place )
+  {
+    Debug.Log( $"Ви сіли на {idx_of_place} місце" );
+
+    changeColorPlace( idx_of_place );
+    gameObject.SetActive( false );// Тушим панель
+  }
+
+  private void changeColorPlace( int idx_of_place )
+  {
+    setAllPlacesGreen();
+    btn_places[idx_of_place].GetComponent<Image>().color = Color.black;
+  }
+
+  private void setAllPlacesGreen()
+  {
+    foreach ( Button btn in btn_places )
+      btn.GetComponent<Image>().color = Color.green;
+  }
+}
