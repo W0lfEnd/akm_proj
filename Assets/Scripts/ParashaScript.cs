4dab8dd0c188c36dc5b8939c2d8b9ceebd6552df
Andrii <oliinyk.dev@gmail.com>
2019-11-21 20:21:53 +0200

    aded parasha script

---
+++ Assets/Scripts/ParashaScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ParashaScript : MonoBehaviour
+{
+    [SerializeField] public TMP_InputField textName;
+
+    public void saveNickname()
+    {
+        CommonData.NickName = textName.text;
+    }
+}
