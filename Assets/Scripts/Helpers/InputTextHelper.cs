c6cfbadbabe807785e79c3860ddbdf3937b76195
Andrii <oliinyk.dev@gmail.com>
2019-11-22 19:59:30 +0200

    added manevr panel

---
+++ Assets/Scripts/Helpers/InputTextHelper.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+
+
+public static class InputTextHelper
+{
+  public static string getInputName( GameController.ButtonActionType type )
+  {
+    switch ( type )
+    {
+      case GameController.ButtonActionType.ComboManeuver1Id: return "☆";
+      case GameController.ButtonActionType.ComboManeuver2Id: return "☈";
+      case GameController.ButtonActionType.ComboManeuver3Id: return "☘";
+      case GameController.ButtonActionType.ComboManeuver4Id: return "☯";
+      case GameController.ButtonActionType.ComboManeuver5Id: return "♆";
+      case GameController.ButtonActionType.ComboManeuver6Id: return "⛛";
+    }
+
+    return "⛱";
+  }
+}
---
+++ Assets/Scripts/UI/MeteorColisionHintPanel/MeteorColisionHintsPanel.cs
@@ -0,0 +1,39 @@
+﻿using Model;
+using TMPro;
+using UnityEngine;
+
+public class MeteorColisionHintsPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject meteor_hint_panel;
+    [SerializeField] private TextMeshProUGUI meteor_combo_hint;
+
+    private void Update()
+    {
+        if ( Client.client == null || Client.client.Map == null )
+            return;
+
+        MeteorData nearest_meteor = Client.client.Map.meteorsData[Client.client.Model.iteration.Value];
+        int hint_time = 0;
+        switch ( nearest_meteor.size )
+        {
+            case 0: hint_time = 6; break;
+            case 1: hint_time = 9; break;
+            case 2: hint_time = 12; break;
+        }
+        
+        if ( nearest_meteor.timeSeconds - Client.client.Model.currentTime.Value < hint_time )
+        {
+            meteor_hint_panel.SetActive( true );
+            foreach ( var VARIABLE in nearest_meteor.combo )
+            {
+                
+            }
+        }
+        else
+        {
+            meteor_hint_panel.SetActive( false );
+        }
+    }
+    
+    
+}
