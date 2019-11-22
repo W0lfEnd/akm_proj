a0c96ca43ba3922ca7ee97f47ffa4b9d358c5eb6
Max Verchinskiy <disablaksteam@gmail.com>
2019-11-22 22:56:13 +0200

    SUKAAAAAAAAAAAAAA

---
+++ Assets/SpaceShipManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipManager : MonoBehaviour
+{
+  public Animator ship_anim;
+
+  public void Start()
+  {
+  }
+
+  public void destroyShip()
+  {
+    ship_anim.SetTrigger( "Explosion" );
+  }
+  
+  public void miss()
+  {
+    ship_anim.SetTrigger( "Miss" );
+  }
+  
+  public void destroyByMeteor()
+  {
+    ship_anim.SetTrigger( "ExploseByMeteor" );
+  }
+  
+  public void meteorDamage()
+  {
+    ship_anim.SetTrigger( "MeteorDamage" );
+  }
+}
