37072d8d8ae42caf5c245c7284a1486a1abceba8
Kostiantyn Semeniuk <constantinsd@gmail.com>
2019-12-04 23:49:51 +0200

    added pnlPrepareGame

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,31 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
 {
-   [SerializeField] private GameObject main_menu;
-   [SerializeField] private GameObject create_game_menu;
-   [SerializeField] private GameObject join_to_game_menu;
+    [SerializeField] private GameObject main_menu;
+    [SerializeField] private GameObject create_game_menu;
+    [SerializeField] private GameObject join_to_game_menu;
+    [SerializeField] private GameObject prepare_game_menu;
 
-   public void goToMainMenu()
-   {
-      main_menu.SetActive( true );
-      create_game_menu.SetActive( false );
-      join_to_game_menu.SetActive( false );
-   }
-   
-   public void goToCreateGameMenu()
-   {
-      main_menu.SetActive( false );
-      create_game_menu.SetActive( true );
-      join_to_game_menu.SetActive( false );
-   }
-   
-   public void goToJoinToGameMenu()
-   {
-      main_menu.SetActive( false );
-      create_game_menu.SetActive( false );
-      join_to_game_menu.SetActive( true );
-   }
+    public void goToMainMenu()
+    {
+        main_menu.SetActive(true);
+        create_game_menu.SetActive(false);
+        join_to_game_menu.SetActive(false);
+        prepare_game_menu.SetActive(false);
+    }
+
+    public void goToCreateGameMenu()
+    {
+        main_menu.SetActive(false);
+        create_game_menu.SetActive(true);
+        join_to_game_menu.SetActive(false);
+        prepare_game_menu.SetActive(false);
+    }
+
+    public void goToJoinToGameMenu()
+    {
+        main_menu.SetActive(false);
+        create_game_menu.SetActive(false);
+        join_to_game_menu.SetActive(true);
+        prepare_game_menu.SetActive(false);
+    }
+
+    public void goToPrepareGameMenu()
+    {
+        main_menu.SetActive(false);
+        create_game_menu.SetActive(false);
+        join_to_game_menu.SetActive(false);
+        prepare_game_menu.SetActive(true);
+    }
 }
